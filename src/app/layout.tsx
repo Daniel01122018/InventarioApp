dfa255783852c5f7e22569c0802816e3bd8bd26e
DanielCL <danielcabrera1998yt@gmail.com>
2025-11-10 22:37:47 +0000

    agrega pwa para poder instalar la aplicación

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,7 @@ setDefaultOptions({ locale: es });
 export const metadata: Metadata = {
   title: 'Tu Inventario',
   description: 'Gestiona las fechas de caducidad de tus productos con facilidad.',
+  manifest: '/manifest.json',
 };
 
 export default function RootLayout({
@@ -23,6 +24,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
+        <meta name="theme-color" content="#5a9a5a" />
       </head>
       <body className="font-body antialiased">
         {children}
